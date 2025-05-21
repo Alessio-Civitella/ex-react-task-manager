91a053f0abceda568cf76ffe482cb15cfa2d5d61
Alessio-Civitella <alessio.civitella19@gmail.com>
2025-05-21 12:13:31 +0200

    Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"
 import AddTask from "./pages/AddTask"
 import TaskList from "./pages/TaskList"
 import "./App.css";
-
+import { GlobalProvider } from "./context/GlobalContext";
 
 function App() {
 
 
   return (
+    <GlobalProvider>
     <BrowserRouter>
     <nav>
       <NavLink to="/">Lista Task</NavLink>
@@ -18,6 +19,7 @@ function App() {
         <Route path="/add" element={<AddTask/>} />
       </Routes>
     </BrowserRouter>
+    </GlobalProvider>
   )
 }
 
--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,4 +1,13 @@
+import { useContext, usecontext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
+
 export default function TaskList() {
+
+    const {tasks} = useContext(GlobalContext);
+    console.log('Tasks:', tasks);
+    
+
+
   return (
     <div className="task-list-container">
       <h1>Lista delle Task</h1>
